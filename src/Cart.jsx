598e19e6c90c71239e054477abebf6b5e4a45aec
bigbarufa <paulo.matos16@gmail.com>
2026-02-10 14:37:58 +0000

    Developed and styled Cart.jsx

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -4,7 +4,7 @@ import CartCard from "./CartCard.jsx";
 
 function Cart({ cart, editCart, plusMinusCart, removeFromCart }) {
   const cartQtt = cart.reduce((a, b) => a + parseInt(b.qtt), 0);
-  //const cartTotal = cart.reduce((a, b) => a + parseFloat(b.price * b.qtt), 0)
+  const cartTotal = cart.reduce((a, b) => a + parseFloat(b.price * b.qtt), 0)
 
   const cartList = cart.map((item) => {
     return (
@@ -24,11 +24,33 @@ function Cart({ cart, editCart, plusMinusCart, removeFromCart }) {
     );
   });
 
+  const cartTotalList = cart.map((item) => {
+    return (
+      <>
+        <p className="cart-total-item">{item.title}</p>
+        <p className="cart-total-price">{`${item.qtt}x ${new Intl.NumberFormat(
+                    "en-GB", { style: "currency", currency: "GBP" }
+                ).format(item.price)}`}</p>
+      </>
+    );
+  });
+
   return (
     <>
       <Navbar selected={3} cart={cart} />
       <div className="cart-content">
-        {cartQtt ? <>{cartList}</> : <p>Your cart is empty!</p>}
+        <div className="cart-item-list">
+          {cartQtt ? <>{cartList}</> : <p>Your cart is empty!</p>}
+        </div>
+        <div className="cart-total">
+          <h2 className="cart-title">Cart total:</h2>
+          <hr />
+          {cartTotalList}
+          <hr />
+          <h3 className="cart-total-value">{new Intl.NumberFormat(
+                    "en-GB", { style: "currency", currency: "GBP" }
+                ).format(cartTotal)}</h3>
+        </div>
       </div>
     </>
   );
--- src/CartCard.jsx
+++ src/CartCard.jsx
@@ -4,13 +4,15 @@ import "./styles/CartCard.css"
 
 function CartCard({id, cart, editCart, plusMinusCart, removeFromCart, image, title, price, description}) {
 
+    const shortDescription = description.length > 150 ? description.substring(0, 150) + "..." : description
+
     return (
         <div className="cart-card">
             <div className="left-wrapper">
                 <img className="cart-card-img" src={image} />
                 <div className="product-text-wrapper">
                     <p className="product-title">{title}</p>
-                    <p className="product-description">{description}</p>
+                    <p className="product-description">{shortDescription}</p>
                 </div>
             </div>
             <div className="right-wrapper">
