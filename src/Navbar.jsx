584920c8c29d303125bb18d4ec826498caf22abb
bigbarufa <paulo.matos16@gmail.com>
2026-02-18 16:35:07 +0000

    Added responsiveness to menu with dropdown

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,10 +1,14 @@
 import "./styles/Navbar.css"
+import { useState } from "react"
 import logoImg from "./assets/cart-logo.png"
+import menuImg from "./assets/menu.svg"
 import { Link } from "react-router"
 
 function Navbar({selected, cart}) {
 
   const cartQtt = cart.reduce((a, b) => a + parseInt(b.qtt), 0)
+  
+  const [responsive, setResponsive] = useState(false)
 
   return (
     <>
@@ -14,12 +18,15 @@ function Navbar({selected, cart}) {
             <img src={logoImg} className="cart-logo" />
             <p className="text-logo">ShoppyShop</p>
           </div>
-          <nav className="nav-bar">
+          <nav className={`nav-bar ${responsive && "responsive"}`}>
             <Link className={`nav-item ${selected == 1 && "nav-selected"}`} to="/">Home</Link>
             <Link className={`nav-item ${selected == 2 && "nav-selected"}`} to="../shop">Shop</Link>
             <Link className={`nav-item ${selected == 3 && "nav-selected"}`} to="../cart">Cart {
               cartQtt > 0 && <p className="cart-qtt">{cartQtt}</p>
             }</Link>
+            <div className="nav-menu" onClick={() => setResponsive(!responsive)}>
+              <img src={menuImg} className="menu-icon" />
+            </div>
           </nav>
         </div>
       </div>
